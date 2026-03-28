ab343a76bad0fddb08a23fa7eb18b32315705e7b
Eshit Gandhi <eshitgandhi20012005@gmail.com>
2026-03-28 12:15:56 +0530

    Fix Render deployment cache bug for Puppeteer by placing chrome in local project cache

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -4,6 +4,7 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
+  // Changes the cache location for Puppeteer to reside in the app directory
+  // so that Render doesn't lose the Chrome executable between the build and start stages
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
